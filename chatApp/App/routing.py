1d454fad33e8623491e1ba78805a9bd233f7885e
quannguyen1234 <84028020+quannguyen1234@users.noreply.github.com>
2022-07-12 09:55:44 +0700

    ..

--- chatApp/App/routing.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .import consumer
-
-websocket_urlpatterns=[ 
-    path('ws/socket-server/',consumer.chatConsumer.as_asgi())                      
-
-]
--- chatApp/test.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-
-async def count():
-    print("one")
-    # await()
-    
-count()
-
-print("hello")
